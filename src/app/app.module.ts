ca1453645672b2cee5e25dc61ceb75246b9c0d04
iamjigz <jigzimon@gmail.com>
2019-07-02 18:15:19 +0800

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
 
@@ -15,22 +16,30 @@ import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './store/effects/app.effects';
 
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 // Components
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './routes/_partial/navigation/navigation.component';
 import { MainComponent } from './routes/main/main.component';
+import { InventoryFormComponent } from './routes/inventory/inventory-form/inventory-form.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, MainComponent],
+  declarations: [AppComponent, NavigationComponent, MainComponent, InventoryFormComponent],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     EffectsModule.forRoot([AppEffects]),
-    StoreRouterConnectingModule.forRoot()
+    StoreRouterConnectingModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
