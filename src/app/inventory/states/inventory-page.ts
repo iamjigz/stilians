bf6b50ce9ab1d060ce4bc7e433eb0f4c735be2c3
iamjigz <jigzimon@gmail.com>
2019-07-22 19:52:15 +0800

    Add stock info

--- src/app/inventory/states/inventory-page.ts
+++ src/app/inventory/states/inventory-page.ts
@@ -1,7 +1,8 @@
-import { Item } from './../models/item';
+import { Item, Stock } from './../models/item';
 
 export interface InventoryPage {
   loading: boolean;
   items: Item[];
   formStatus: string;
+  stock: Stock[];
 }
